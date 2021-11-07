3024ee1f8e382e6a6e87d3052be9a3907e9cd698
Mityal999 <litvinenko-mitya@yandex.ru>
2021-11-07 19:20:42 +0700

    Локация 2

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
